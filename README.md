6e4a45141b0dbfd908903a9dd41afca675ddd2e8
Richard Bramley <rabramley@gmail.com>
2016-11-15 15:45:28 +0000

    Initial check in

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # GENVASC Portal Holding Page
 
 A holding page for the GENVASC portal
+
+## Running
+
+### In Dev
+
+docker-compose up -d
+
+### In Production
+
+docker-compose -f docker-compose.yml -f production.yml up -d
+
---
+++ app/portal/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, request
+from portal.config import BaseConfig
+
+app = Flask(__name__)
+app.config.from_object(BaseConfig)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html', ip_address = request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
