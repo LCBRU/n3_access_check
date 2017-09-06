58f03a9f6e515c8fbc15b516b395739b0e923618
Richard Bramley <rabramley@gmail.com>
2017-09-06 09:41:54 +0100

    Convert to generic N3 access check

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# GENVASC Portal Holding Page
+# Your IP Address
 
-A holding page for the GENVASC portal
+This simple tool has two jobs:
+
+1. Allow people in NHS organisations to check that they can view web pages on N3
+2. Allow them to get their IP address as seen from within N3
 
 ## Running
 
--- app/portal/__init__.py
+++ app/portal/__init__.py
@@ -4,6 +4,10 @@ from portal.config import BaseConfig
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
 
+
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', ip_address = request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
+    return render_template(
+        'index.html',
+        ip_address=request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    )
